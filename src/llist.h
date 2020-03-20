da5290dea4ba667b81e5365743294b4e1942373d
James McRoberts <james@B97.local>
2020-03-19 22:45:50 -0700

    Moved a value to a struct.

--- src/llist.h
+++ src/llist.h
@@ -12,12 +12,13 @@ struct _llist_node {
 
 struct llist {
     size_t size;
+    bool circular;
 
     struct _llist_node* head;
     struct _llist_node* tail;
 };
 
-bool llist_isCircular(struct llist* list);
+e97_int llist_check(struct llist* list);
 e97_int llist_add(struct llist* list, void* data, size_t index);
 e97_int llist_append(struct llist* list, void* data);
 e97_int llist_remove(struct llist* list, size_t index);
