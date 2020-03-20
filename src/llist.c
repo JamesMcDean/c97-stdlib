9fde49bfb9ba738f78b0a64eeccffcbae669dfe3
James McRoberts <james@B97.local>
2020-03-20 15:34:38 -0700

    Linked list should be complete.

--- src/llist.c
+++ src/llist.c
@@ -80,7 +80,7 @@ e97_int llist_check(struct llist* list) {
     return __llist_check(list, NULL, -1);
 }
 
-e97_int llist_add(struct llist* list, void* data, size_t index) {
+e97_int llist_insert(struct llist* list, void* data, size_t index) {
     e97_int result = E97_NONE;
 
     // Appending
@@ -93,25 +93,188 @@ e97_int llist_add(struct llist* list, void* data, size_t index) {
         struct _llist_node* newNode = calloc(1, sizeof(struct _llist_node));
         newNode->data = data;
         
-        // TODO - Fix all below for zero case
-        // Link to next if circular
-        if (list->circular) {
-            list->tail->next->prev = newNode;
-            newNode->next = list->tail->next;
-        }
+        // Empty list
+        if (list->size == 0) {
+            // Link to root
+            list->head = newNode;
+            list->tail = newNode;
 
-        // Link to previous
-        newNode->prev = list->tail;
-        list->tail->next = newNode;
-        list->tail = newNode;
+            // Link self if circular
+            if (list->circular) {
+                newNode->next = newNode;
+                newNode->prev = newNode;
+            }
+        }
+        // Not empty list
+        else {
+            // Link to next if circular
+            if (list->circular) {
+                list->tail->next->prev = newNode;
+                newNode->next = list->tail->next;
+            }
+
+            // Link to previous
+            newNode->prev = list->tail;
+            list->tail->next = newNode;
+            list->tail = newNode;
+        }
     }
     // Inserting at 0
     else if (index == 0) {
         // Error checking
-        struct _llist_node* target;
-        result = __llist_check(list, &target, 0);
+        struct _llist_node* temp;
+        result = __llist_check(list, &temp, 0);
         if (result < 0) return result;
 
+        // Build node
+        struct _llist_node* newNode = calloc(1, sizeof(struct _llist_node));
+        newNode->data = data;
 
+        // Link
+        if (list->circular) {
+            newNode->prev = list->tail;
+            list->tail->next = newNode;
+            list->tail = newNode;
+        }
+
+        // Shwoop
+        newNode->next = list->head;
+        list->head->prev = newNode;
+        list->head = newNode;
     }
+    // Inserting in general
+    else {
+        // Get & test
+        struct _llist_node* temp;
+        result = __llist_check(list, &temp, index);
+        if (result < 0) return result;
+
+        // Build
+        struct _llist_node* newNode = calloc(1, sizeof(struct _llist_node));
+        newNode->data = data;
+
+        // Link
+        newNode->prev = temp->prev;
+        temp->prev->next = newNode;
+        temp->prev = newNode;
+        newNode->next = temp;
+    }
+
+    // Finish up
+    list->size += 1;
+    return result;
+}
+
+e97_int llist_append(struct llist* list, void* data) {
+    return llist_insert(list, data, list->size);
+}
+
+e97_int llist_remove(struct llist* list, size_t index, void** data) {
+    // Get and error check
+    if (list->size == 0) {
+        sprintf(E97_ERRSTR, "Error: Nothing to remove, llist is empty.");
+        return E97_LLIST_EMPTY;
+    } 
+
+    struct _llist_node* current;
+    e97_int result = __llist_check(list, &current, index);
+    if (result < 0) return result;
+
+    // Unlink
+    if (data != NULL) *data = current->data;
+    // Final remove
+    if (list->size == 1) {
+        free(current);
+        list->head = NULL;
+        list->tail = NULL;
+    }
+    // Remove tail
+    else if (index == list->size - 1) {
+        list->tail = current->prev;
+
+        if (list->circular) {
+            list->head->prev = list->tail;
+            list->tail->next = list->head;
+        }
+        else {
+            list->tail->next = NULL;
+        }
+
+        free(current);
+    }
+    // Remove regular
+    else {
+        current->prev->next = current->next;
+        current->next->prev = current->prev;
+
+        free(current);
+    }
+
+    list->size -= 1;
+    return result;
+}
+
+e97_int llist_pop(struct llist* list, void** data) {
+    return llist_remove(list, list->size - 1, data);
+}
+
+e97_int free_llistPrecise(struct llist* list, struct llist* freeList) {
+    // Error checking
+    if (free_llistPrecise == NULL) return free_llist(list, false);
+    if (list == NULL) {
+        sprintf(E97_ERRSTR, __common_errors(E97_ARGUMENT_NULL));
+        return E97_ARGUMENT_NULL;
+    }
+    if (list->size != freeList->size) {
+        sprintf(E97_ERRSTR, 
+            "Error: param(list) and param(freeList) must be the same size.");
+        return E97_ARGUMENT_ERROR;
+    }
+
+    // Running data
+    e97_int result = E97_NONE;
+    void* data;
+    struct _llist_node* current = NULL;
+
+    // More error checking
+    if ((result = __llist_check(freeList, NULL, -1)) < 0) {
+        return result;
+    }
+    current = freeList->head;
+
+    // Free data
+    while (current != NULL && (result |= llist_remove(list, 0, NULL)) >= 0) {
+        if ((result = llist_remove(list, 0, &data)) < 0) {
+            return result;
+        }
+
+        if (data != NULL && *((bool*) current->data)) {
+            free(data);
+        }
+        current = current->next;
+    }
+
+    return result;
+}
+
+e97_int free_llist(struct llist* list, bool freeData) {
+    // Running data
+    e97_int result = E97_NONE;
+    void* data;
+    size_t dataIndex = list->size;
+
+    // Free data
+    if (freeData) {
+        while (dataIndex > 0 && (result |= llist_remove(list, 0, &data)) >= 0) {
+            free(data);
+            dataIndex -= 1;
+        }
+    }
+    else {
+        while (dataIndex > 0 && (result |= llist_remove(list, 0, NULL)) >= 0) {
+            dataIndex -= 1;
+        }
+    }
+
+    return result;
 }
--- src/llist.h
+++ src/llist.h
@@ -3,6 +3,9 @@
 
 #include "_c97_stdinclude.h"
 
+/*
+The node structure involved in a doubly linked list.
+*/
 struct _llist_node {
     void* data;
 
@@ -10,6 +13,9 @@ struct _llist_node {
     struct _llist_node* prev;
 };
 
+/*
+A linked linked list metadata structure to be operated on as a linked list.
+*/
 struct llist {
     size_t size;
     bool circular;
@@ -18,11 +24,45 @@ struct llist {
     struct _llist_node* tail;
 };
 
+/*
+Checks to make sure that the list is properly built.
+*/
 e97_int llist_check(struct llist* list);
-e97_int llist_add(struct llist* list, void* data, size_t index);
+
+/*
+Stores a piece of data to the index provided.
+*/
+e97_int llist_insert(struct llist* list, void* data, size_t index);
+
+/*
+Adds a piece of data to the end of the linked list.
+*/
 e97_int llist_append(struct llist* list, void* data);
-e97_int llist_remove(struct llist* list, size_t index);
+
+/*
+Stores a piece of data at the index provided into {param:}data, removing it from
+the linked list.
+*/
+e97_int llist_remove(struct llist* list, size_t index, void** data);
+
+/*
+Stores the end piece of data from the linked list into {param:}data, removing it
+from the linked list.
+*/
+e97_int llist_pop(struct llist* list, void** data);
+
+/*
+Frees the linked list, optionally freeing each item in the list. There are a
+few notes on operation.
+[{param:}freeList->size == {param:}list->size] or an error will return
+[{param:}freeList->[node]->(bool*)data] is how each value is stored
+{param:}freeList is checked for proper structure
+*/
+e97_int free_llistPrecise(struct llist* list, struct llist* freeList);
+
+/*
+Frees the linked list, optionally freeing all data stored in the list.
+*/
 e97_int free_llist(struct llist* list, bool freeData);
-e97_int free_llist_precise(struct llist* list, struct llist* freeList);
 
 #endif
