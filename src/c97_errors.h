75352864f32401df2f1e09f1f3a456ba67146816
James McRoberts <jamesmcroberts82997@gmail.com>
2020-03-26 06:56:37 +0000

    This should fix the overflow warning.

--- src/c97_errors.h
+++ src/c97_errors.h
@@ -12,17 +12,34 @@ typedef int64_t e97_int;
 
 // Error String definitions
 #define E97_ERROR_STRING_MAX_CHAR 4 * (1 << 10) - 1
+
 static char E97_ERRSTR[E97_ERROR_STRING_MAX_CHAR + 1] = {'\0'};
 static bool E97_BLOCKCLEAR = false;
+
 #define E97_ERRSTR_CLR() { E97_ERRSTR[0] = '\0'; }
 #define E97_ERRSTR_ISCLR() (E97_ERRSTR[0] == '\0')
-#define E97_ERRSTR_WRITE(newErr) {\
-    if (E97_ERRSTR_ISCLR() && !E97_BLOCKCLEAR) {\
-        sprintf(E97_ERRSTR, "%s", (newErr));\
-    }\
-    else {\
-        sprintf(E97_ERRSTR, "%s: %s", E97_ERRSTR, (newErr));\
-    }\
+
+static inline void E97_ERRSTR_WRITE(char* error) {
+    // Quick write
+    if (E97_ERRSTR_ISCLR() && !E97_BLOCKCLEAR) {
+        sprintf(E97_ERRSTR, "%s", error);
+        return;
+    }
+
+    // Write onto the end of the string
+    // Running data
+    size_t start = strlen(E97_ERRSTR);
+    size_t argLength = strlen(error);
+
+    for (size_t index = start, jndex = 0; index < E97_ERROR_STRING_MAX_CHAR 
+        && jndex < argLength;) {
+        // Copy char
+        E97_ERRSTR[index] = error[jndex];
+
+        // Increment
+        index += 1;
+        jndex += 1;
+    }
 }
 
 // Error definitions
