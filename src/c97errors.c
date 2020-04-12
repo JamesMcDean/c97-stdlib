d2a3d8d614b147b5bbfa0bdd1089758be3403688
James McRoberts <jamesmcroberts82997@gmail.com>
2020-04-11 18:48:27 -0700

    Missed some types refactoring.

--- src/c97errors.c
+++ src/c97errors.c
@@ -8,25 +8,25 @@ char* E97_ERRSTR() {
 }
 
 bool* E97_BLOCKCLEAR() {
-    return _E97_BLOCKCLEAR;
+    return &_E97_BLOCKCLEAR;
 }
 
 void E97_ERRSTR_WRITE(char* error) {
     // Quick write
     if (E97_ERRSTR_ISCLR()) {
-        sprintf(_E97_ERRSTR, "%s", error);
+        sprintf(E97_ERRSTR(), "%s", error);
         return;
     }
 
     // Write onto the end of the string
     // Running data
-    size_t start = strlen(_E97_ERRSTR);
+    size_t start = strlen(E97_ERRSTR());
     size_t argLength = strlen(error);
 
     for (size_t index = start, jndex = 0; index < E97_ERROR_STRING_MAX_CHAR 
         && jndex < argLength;) {
         // Copy char
-        _E97_ERRSTR[index] = error[jndex];
+        E97_ERRSTR()[index] = error[jndex];
 
         // Increment
         index += 1;
